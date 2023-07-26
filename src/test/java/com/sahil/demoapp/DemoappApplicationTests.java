872b6c0866d7ff330d261e1778b1c950248a39f2
sahil193101 <sahillone193101@gmail.com>
2023-07-26 05:18:28 +0000

    initial commit

---
+++ src/test/java/com/sahil/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
